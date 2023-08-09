ddfc4f68c3edaf82bc37d164d00e2505812900cc
thanhsangvo <pl.thanhsang0710@gmail.com>
2023-08-09 09:27:12 +0700

    Initial Commit

---
+++ BeReal_Clone/BeReal_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeReal_CloneApp.swift
+//  BeReal_Clone
+//
+//  Created by Dev Sang on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeReal_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
